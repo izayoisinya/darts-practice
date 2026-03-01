9acc6e3e254350887ed6d8ca52cb4026f211c335
十六夜真夜 <izayoisinya0124@gmail.com>
2026-03-02 07:12:44 +0900

    update

--- script.js
+++ script.js
@@ -571,4 +571,14 @@ document
   .addEventListener("click", resetGame);
   
   
+  if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js")
+    .then(() => console.log("SW registered"))
+    .catch(err => console.log("SW error", err));
+}
+  
+  
+  
+  
+  
   

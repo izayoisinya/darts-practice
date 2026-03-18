bf63df92ea7eded53e2931532a9d4369e0ba7f85
十六夜真夜 <izayoisinya0124@gmail.com>
2026-03-19 02:36:40 +0900

    update

--- js/date.js
+++ js/date.js
@@ -49,3 +49,49 @@ function addStat(title, value) {
 }
 
 loadStats()
+
+
+function loadSessions() {
+  
+  const sessions = JSON.parse(
+    localStorage.getItem("dartsSessions")
+  ) || []
+  
+  const container = document.getElementById("dataContainer")
+  
+  container.innerHTML = ""
+  
+  if (!sessions.length) {
+    container.innerHTML = "<p>No data</p>"
+    return
+  }
+  
+  // 新しい順
+  sessions.slice().reverse().forEach((s, index) => {
+    
+    const div = document.createElement("div")
+    
+    div.style.marginBottom = "20px"
+    div.style.padding = "10px"
+    div.style.border = "1px solid #333"
+    
+    div.innerHTML = `
+
+    <div>Game ${sessions.length - index}</div>
+
+    <div>Score: ${s.score}</div>
+    <div>PPD: ${s.ppd}</div>
+    <div>Date: ${new Date(s.date).toLocaleString()}</div>
+
+    <div>Rounds:</div>
+    <pre>${JSON.stringify(s.rounds, null, 2)}</pre>
+
+  `
+    
+    container.appendChild(div)
+    
+  })
+  
+}
+
+loadSessions()
